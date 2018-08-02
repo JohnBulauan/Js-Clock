dd8af279bba7663fe1b0b322abe1561863712445
John Bulauan <John.bulauan@hotmail.com>
2018-08-02 16:39:33 +1000

    added splash screen, layouts portrait and landscape, Added intent creation for when use clicks on button to move to the second activity

---
+++ JsClock/AddAClockActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace JsClock
+{
+    [Activity(Label = "AddAClockActivity")]
+    public class AddAClockActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
